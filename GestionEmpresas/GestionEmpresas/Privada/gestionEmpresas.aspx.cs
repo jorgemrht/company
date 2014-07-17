b5951e7950bd525b825bb79bdf46b4e23cab60c2
jorgemrht <git@jorgemrht.dev>
2014-07-17 12:11:04 +0200

    Arreglado DeleteDireccion

--- GestionEmpresas/GestionEmpresas/Privada/gestionEmpresas.aspx.cs
+++ GestionEmpresas/GestionEmpresas/Privada/gestionEmpresas.aspx.cs
@@ -162,7 +162,7 @@ namespace GestionEmpresas.Privada
             EmpresaData emp = empresas[gvEmpresas.SelectedIndex];
             var direcciones = proxy.getDirecionesEmpresa(emp.EmpresaID);
             DireccionData dir = direcciones[e.RowIndex];
-            proxy.DeleteDireccion(dir, dir.idDireccion);
+            proxy.DeleteDireccion(dir.idDireccion);
             Response.Redirect("~/Privada/gestionEmpresas.aspx");
         }
 
