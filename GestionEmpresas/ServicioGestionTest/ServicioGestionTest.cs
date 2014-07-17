561e394555ed5741d03453283fb76dfc0ddcbc38
jorgemrht <git@jorgemrht.dev>
2014-07-17 10:33:34 +0200

    cambios rápidos

--- GestionEmpresas/ServicioGestionTest/ServicioGestionTest.cs
+++ GestionEmpresas/ServicioGestionTest/ServicioGestionTest.cs
@@ -41,7 +41,7 @@ namespace ServicioGestionTestSpace.ServiceReference1
         public void AddAccionComercialTest()
         {
 
-            AccionComercialData accion = new AccionComercialData() { descripcion = "prueba", fechaHora = new DateTime() };
+            AccionComercialData accion = new AccionComercialData() { descripcion = "prueba", fechaHora = new DateTime(2000, 12, 02) };
             int nueva = proxy.addAccionComercial(accion);
             Assert.IsTrue(accion.descripcion == proxy.getAccionComercial(nueva).descripcion);
             proxy.deleteAccionComercial(nueva);
@@ -248,7 +248,7 @@ namespace ServicioGestionTestSpace.ServiceReference1
         {
             Assert.IsTrue(proxy.addAccionComercial(null) == -1);
 
-            AccionComercialData accion1 = new AccionComercialData() { descripcion = "prueba1", fechaHora = new DateTime() };
+            AccionComercialData accion1 = new AccionComercialData() { descripcion = "prueba1", fechaHora = new DateTime(2000, 12, 02) };
             int nueva = proxy.addAccionComercial(accion1);
             accion1.idAccion = nueva;
             accion1.descripcion = "cambioprueba1";
@@ -352,7 +352,7 @@ namespace ServicioGestionTestSpace.ServiceReference1
         [TestMethod]
         public void DeleteAccionComercialTest()
         {
-            int nueva = proxy.addAccionComercial(new AccionComercialData() { descripcion = "prueba", fechaHora = new DateTime() });
+            int nueva = proxy.addAccionComercial(new AccionComercialData() { descripcion = "prueba", fechaHora = new DateTime(2000,12,02) });
             int registros = proxy.getAllAccionesComerciales().Length;
             Assert.IsTrue(proxy.deleteAccionComercial(nueva));
             Assert.IsTrue(proxy.getAllAccionesComerciales().Length == registros - 1);
@@ -468,7 +468,7 @@ namespace ServicioGestionTestSpace.ServiceReference1
         [TestMethod]
         public void GetAccionComercialTest()
         {
-            int nueva = proxy.addAccionComercial(new AccionComercialData() { descripcion = "prueba", fechaHora = new DateTime() });
+            int nueva = proxy.addAccionComercial(new AccionComercialData() { descripcion = "prueba", fechaHora = new DateTime(2000, 12, 02) });
             Assert.IsTrue(proxy.getAccionComercial(nueva).descripcion == "prueba");
             proxy.deleteAccionComercial(nueva);
         }
@@ -579,11 +579,21 @@ namespace ServicioGestionTestSpace.ServiceReference1
         public void GetAllAccionesComercialTest()
         {
             int registros = proxy.getAllAccionesComerciales().Length;
-            int nueva = proxy.addAccionComercial(new AccionComercialData() { descripcion = "prueba", fechaHora = new DateTime() });
+            int nueva = proxy.addAccionComercial(new AccionComercialData() { descripcion = "prueba", fechaHora = new DateTime(2000, 12, 02) });
             Assert.IsTrue(proxy.getAllAccionesComerciales().Length == registros + 1);
             proxy.deleteAccionComercial(nueva);
         }
-        
+        /*
+        public void GetAccionesComercialesUsuarioTest()
+        {
+
+        }
+
+        public void GetAccionesComercialesEmpresaTest()
+        {
+
+        }
+        */
         [TestMethod]
         public void GetAllTipoAccionTest()
         {
