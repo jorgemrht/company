0438aa3895b7006d3e22f1e4f7f1ffa07558ecf9
jorgemrht <git@jorgemrht.dev>
2014-07-23 11:10:44 +0200

    Modificacion del editar direccion, queda piendite de mejorar

--- GestionEmpresas/GestionEmpresas/Privada/editDireccion.aspx.cs
+++ GestionEmpresas/GestionEmpresas/Privada/editDireccion.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GestionEmpresas.srvGestion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,14 +12,10 @@ namespace GestionEmpresas.Privada
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
+            int idDireccion = Convert.ToInt32(Request.QueryString["id"]);
+            ServicioGestionClient proxy = new ServicioGestionClient();
 
-            }
-            else
-            {
-
-            }
+            var direccion = proxy.GetDireccion();
         }
 
         protected void editDirec(object sender, EventArgs e)
@@ -27,17 +24,66 @@ namespace GestionEmpresas.Privada
             if (this.IsPostBack)
             {
                 this.Validate();
+
                 if (this.IsValid)
                 {
-                    Response.Redirect(".aspx");
-                }// Fin del if
-            }// Fin del if
+
+                    ServicioGestionClient proxy = new ServicioGestionClient();
+
+                    // En estas dos variables almacenamos el numero del idempresa o idcontacto y un cero.
+                    int idDireccion = Convert.ToInt32(Request.QueryString["id"]);
+                    int res = -1; // Inicializamos el resultado a valor -1
+
+                    /** Objeto direccion **/
+
+                    DireccionData objetoStreet = new DireccionData();
+
+                    objetoStreet.idDireccion = idDireccion;
+                    objetoStreet.domicilio = this.domici.Text;
+                    objetoStreet.poblacion = this.poblac.Text;
+                    objetoStreet.codPostal = this.cp.Text;
+                    objetoStreet.provincia = this.provin.Text;
+
+                    /** Fin objeto direccion **/
+
+                    /***************************************************************************************************************************/
+
+                    // Si la empresa es distinto a cero, procedemos a guardar los datos a la BD.
+                    if (idDireccion != 0)
+                    {
+                        // Obtengo el objeto empresa
+                        res = proxy.EditDireccion(objetoStreet);
+                    }
+
+
+
+                    /***************************************************************************************************************************/
+
+                    // Si el resultado que nos devuelve el servicio es != -1 llevamos al usuario a una web ( GestionEmpresa o GestionContacto )
+                    if (res != -1)
+                    {
+                        if (res != 0) Response.Redirect("Default.aspx");
+                    }
+                    // Si el resultado que nos devuelve la BD no es valido, mostraremos un error en el formulario
+                    else
+                    {
+                        //this.lblError.Text = "No se guardaron los datos, error de acceso al servicio";
+                        //this.alert.Visible = true;
+                    }
+
+                    /***************************************************************************************************************************/
+                } // Fin del if (this.IsValid)
+            }// Fin del if (this.IsPostBack)
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Volver(object sender, EventArgs e)
         {
-            Response.Redirect(".aspx", true);
+            Response.Redirect("Default.aspx", true);
         }
     }
 }
